6d145edb7c4a33f4ad942622683ba6aabceef0c7
JohnathanBaldera <74841308+JohnathanBaldera@users.noreply.github.com>
2021-04-13 21:23:41 -0400

    Updated Games.js
    Added fade functionality to random button and sound.

--- game.js
+++ game.js
@@ -1,1 +1,15 @@
-$("h1")
+let gamePattern = [];
+let userClickedPattern = [];
+let buttonColors = ["red", "blue", "green", "yellow"];
+
+function nextSequence() {
+    let randomNumber = Math.floor(Math.random() * 4);
+    let randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn();
+    let sound = new Audio("sounds/" + randomChosenColor + ".mp3");
+    sound.play();
+
+    
+
+};
